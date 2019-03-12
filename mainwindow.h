a658990d84aa4e41ee114822e319b71faccdea82
DongMugua <47498737+DongMugua@users.noreply.github.com>
2019-03-12 08:16:29 +0100

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,1 +1,45 @@
 
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+
+#include <QMainWindow>
+#include <vector>
+
+QT_BEGIN_NAMESPACE
+class QAction;
+class QLabel;
+class QMenu;
+QT_END_NAMESPACE
+
+
+class MainWindow : public QMainWindow {
+  Q_OBJECT
+
+public:
+  explicit MainWindow(QWidget *parent = nullptr);
+  ~MainWindow() = default;
+
+  QLabel *image_widget_;
+
+private slots:
+  void slot_exit();
+  void slot_about();
+  void slot_load_checker_board_image();
+
+private:
+  void create_menus();
+  void create_actions();
+  int* RGB_Valeur(const double x, const double y);
+  bool Dans_Tri(const double x, const double y);
+
+  QAction *action_checker_board_image_;
+  QAction *action_about_;
+  QAction *action_exit_;
+
+  QMenu *menu_open_;
+  QMenu *menu_help_;
+};
+
+#endif  // MAINWINDOW_H
